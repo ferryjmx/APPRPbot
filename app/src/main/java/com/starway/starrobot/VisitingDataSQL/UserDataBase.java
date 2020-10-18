1441b990f0f2bbc000fbc87769a7349ae558db59
mojie-DH <468420711@qq.com>
2020-10-18 21:28:40 +0800

    限制数据库只允许生成一个实例

--- app/src/main/java/com/starway/starrobot/VisitingDataSQL/UserDataBase.java
+++ app/src/main/java/com/starway/starrobot/VisitingDataSQL/UserDataBase.java
@@ -1,9 +1,20 @@
 package com.starway.starrobot.VisitingDataSQL;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
+//singleton   只允许生成一个实例
 @Database(entities = {UserData.class},version = 1,exportSchema = false)
 public abstract class UserDataBase extends RoomDatabase {
+    private static UserDataBase INSTABCE;//作为一个参数
+    static synchronized UserDataBase getDataBase(Context context){//synchronized 保证线程能够正常运行预防碰撞，
+        if(INSTABCE==null){
+            INSTABCE= Room.databaseBuilder(context.getApplicationContext(),UserDataBase.class,"UserData_DataBase")
+                    .allowMainThreadQueries().build();//强制在主线程运行，并展示
+        }
+        return INSTABCE;
+    }
     public abstract UserDao getUserDao();
 }
-//lasdasd
