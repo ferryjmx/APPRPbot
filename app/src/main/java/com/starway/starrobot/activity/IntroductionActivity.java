52c7ce46e0e70dea971144887fbc3ae49cf167c7
ferryjmx <2778977102@qq.com>
2020-10-22 22:50:19 +0800

    add:IntroductionActivity
    添加用于自动轮播图片展示功能简介的IntroductionActivity(ViewFlipper)

---
+++ app/src/main/java/com/starway/starrobot/activity/IntroductionActivity.java
@@ -0,0 +1,15 @@
+package com.starway.starrobot.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.starway.starrobot.R;
+
+public class IntroductionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introduction);
+    }
+}
